4fcc7f691b3d1b1b79dcaf7664ea43d4b5438aa1
pratyu-j <pratyush021j@gmail.com>
2020-02-26 18:50:27 +0530

    communcation section

--- app/src/main/java/com/example/icscapp/MainActivity.java
+++ app/src/main/java/com/example/icscapp/MainActivity.java
@@ -65,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.schedules, R.id.speakers, R.id.paperStatus,R.id.prevConf , R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
+                R.id.about_us, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
