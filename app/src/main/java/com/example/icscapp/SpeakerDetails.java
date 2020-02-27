cdb040c637fad927b3d458560d1767e55233a3e7
pratyu-j <pratyush021j@gmail.com>
2020-02-27 22:29:25 +0530

    previous editions and speaker details 2

---
+++ app/src/main/java/com/example/icscapp/SpeakerDetails.java
@@ -0,0 +1,43 @@
+package com.example.icscapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class SpeakerDetails extends AppCompatActivity {
+    String name, image, about, from;
+    private TextView speakerName, speakerAbout, speakerFrom;
+    private ImageView speakerImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speaker_details);
+
+
+        Bundle bundle = getIntent().getExtras();
+        name = bundle.getString("NAME");
+        image = bundle.getString("IMAGE");
+        about = bundle.getString("ABOUT");
+        from = bundle.getString("FROM");
+
+
+        speakerName = findViewById(R.id.speakerName);
+        speakerAbout = findViewById(R.id.speakerInfo);
+        speakerFrom = findViewById(R.id.speakerCountry);
+        speakerImg = findViewById(R.id.speakerimg);
+
+        speakerName.setText(name);
+        speakerAbout.setText(about);
+        speakerFrom.setText(from);
+        //speakerImg.setImageBitmap();
+
+
+    }
+}
