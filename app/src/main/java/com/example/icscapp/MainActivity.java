413e8f5f63f626da5341b67fcbf71d946525d024
Rohan Verma <rsrohanverma@gmail.com>
2020-02-28 17:07:36 +0530

    explore ncr

--- app/src/main/java/com/example/icscapp/MainActivity.java
+++ app/src/main/java/com/example/icscapp/MainActivity.java
@@ -44,28 +44,17 @@ public class MainActivity extends AppCompatActivity {
             FirebaseDatabase.getInstance().setPersistenceEnabled(true);
             calledAlready = true;
         }        user = FirebaseAuth.getInstance().getCurrentUser();
-//        if (user==null)
-//        {
-//            startActivity(new Intent(this, LoginActivity.class));
-//            finish();
-//        }
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.schedules, R.id.speakers, R.id.paperStatus,R.id.prevConf , R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.about_us, R.id.nav_share, R.id.nav_navigate, R.id.nav_send)
+                R.id.about_us, R.id.nav_share, R.id.nav_navigate, R.id.nav_send, R.id.nav_explore)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
