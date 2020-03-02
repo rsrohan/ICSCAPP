da9e712735808a8e7295791774caa5f74135a45f
pratyu-j <pratyush021j@gmail.com>
2020-03-02 19:11:15 +0530

    poster and about_us bug fix

---
+++ app/src/main/java/com/jiit/icscapp/poster_data_fragment.java
@@ -0,0 +1,131 @@
+package com.jiit.icscapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+
+public class poster_data_fragment extends Fragment {
+
+
+    DatabaseReference databaseReference;
+    RecyclerView recyclerView;
+    ProgressBar progressBar;
+    private OnFragmentInteractionListener mListener;
+    int day;
+
+
+
+    public poster_data_fragment(int x) {
+        // Required empty public constructor
+        day = x;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_poster_data_fragment, container, false);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("SCHEDULES").child(""+day);
+        databaseReference.keepSynced(true);
+        recyclerView = v.findViewById(R.id.recyclerViewForPosters);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        progressBar = v.findViewById(R.id.progressbar);
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                try{
+                    ArrayList<Schedules> schedulesArrayList = new ArrayList<>();
+                    for(DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+
+                        Log.d(TAG, "onDataChange: "+dataSnapshot1.getValue());
+                        Schedules s = dataSnapshot1.getValue(Schedules.class);
+                        schedulesArrayList.add(s);
+                    }
+                    RecyclerAdapterForSchedules r = new RecyclerAdapterForSchedules(schedulesArrayList, getContext(), getActivity());
+                    r.notifyDataSetChanged();
+                    progressBar.setVisibility(View.GONE);
+
+                    recyclerView.setAdapter(r);
+                }catch (Exception e){}
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+        return v;
+    }
+
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/jiit/icscapp/poster_fragment.java
@@ -0,0 +1,113 @@
+package com.jiit.icscapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class poster_fragment extends Fragment {
+
+    DatabaseReference databaseReference;
+    RecyclerView recyclerView;
+    private ScheduleFragment.OnFragmentInteractionListener mListener;
+
+    public poster_fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_poster_fragment, container, false);
+        ViewPager viewPager = (ViewPager) v.findViewById(R.id.viewpager2);
+        setupViewPager(viewPager);
+        // Set Tabs inside Toolbar
+        TabLayout tabs = (TabLayout) v.findViewById(R.id.result_tabs_poster);
+        tabs.setupWithViewPager(viewPager);
+
+
+
+
+        return v;
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+
+
+        ScheduleFragment.Adapter adapter = new ScheduleFragment.Adapter(getChildFragmentManager());
+        adapter.addFragment( new ScheduleDataFragment(0),"Day 1\nMarch 5");
+        adapter.addFragment(new ScheduleDataFragment(1), "Day 2\nMarch 6");
+
+
+        viewPager.setAdapter(adapter);
+
+
+
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public Adapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+    }
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+
+
+
+}
